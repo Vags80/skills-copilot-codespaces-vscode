e60b51df3d768d9598c6de3c45d2cb96610d3dda
Vags80 <30028697+Vags80@users.noreply.github.com>
2023-07-21 22:03:25 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,66 @@
+// Create web server
+// const http = require('http');
+// const fs = require('fs');
+// const server = http.createServer(function(req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     fs.readFile('index.html', function(error, data) {
+//         if (error) {
+//             res.writeHead(404);
+//             res.write('Error: File Not Found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// });
+// server.listen(3000, function(error) {
+//     if (error) {
+//         console.log('Something went wrong', error);
+//     } else {
+//         console.log('Server is listening on port 3000');
+//     }
+// });
+// const http = require('http');
+// const fs = require('fs');
+// const server = http.createServer(function(req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     fs.readFile('index.html', function(error, data) {
+//         if (error) {
+//             res.writeHead(404);
+//             res.write('Error: File Not Found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// });
+// server.listen(3000, function(error) {
+//     if (error) {
+//         console.log('Something went wrong', error);
+//     } else {
+//         console.log('Server is listening on port 3000');
+//     }
+// });
+// const http = require('http');
+// const fs = require('fs');
+// const server = http.createServer(function(req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     fs.readFile('index.html', function(error, data) {
+//         if (error) {
+//             res.writeHead(404);
+//             res.write('Error: File Not Found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// });
+// server.listen(3000, function(error) {
+//     if (error) {
+//         console.log('Something went wrong', error);
+//     } else {
+//         console.log('Server is listening on port 3000');
+//     }
+// });
+// const http = require('http');
+// const fs = require('fs');
